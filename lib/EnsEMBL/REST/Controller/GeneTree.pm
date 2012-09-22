0659a93ef3b8e575c96af56c8230e7f9312689a9
Andrew Yates <ayates@ebi.ac.uk>
2012-09-22 17:43:28 +0000

    [ENSCORESW-248]. Compara is selected on a more sensible basis based on the species' division

--- lib/EnsEMBL/REST/Controller/GeneTree.pm
+++ lib/EnsEMBL/REST/Controller/GeneTree.pm
@@ -23,8 +23,8 @@ sub get_adaptors : Chained('/') PathPart('genetree') CaptureArgs(0) {
   my ($self, $c) = @_;
   
   my $reg = $c->model('Registry');
-  my $default_compara = EnsEMBL::REST->config()->{Compara}->{default_compara};
-  my $compara_name = $c->request()->param('compara') || $default_compara;
+  my $best_compara_name = $c->model('Registry')->get_compara_name_for_species($c->stash()->{species});
+  my $compara_name = $c->request()->param('compara') || $best_compara_name;
   
   try {
     my $ma = $reg->get_adaptor($compara_name, 'compara', 'member');
