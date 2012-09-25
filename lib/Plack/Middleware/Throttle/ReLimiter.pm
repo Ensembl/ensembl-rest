c3a5658a919c74d45edf1b9be406a7fe7df6afc3
Andrew Yates <ayates@ebi.ac.uk>
2012-09-25 15:10:30 +0000

    Adding in the option to look for the forwarded host and use that as our IP rather than the IP of any load-balancer

--- lib/Plack/Middleware/Throttle/ReLimiter.pm
+++ lib/Plack/Middleware/Throttle/ReLimiter.pm
@@ -17,4 +17,12 @@ sub _create_backend {
   die "backend must be a cache object";
 }
 
+sub client_identifier {
+  my ($self, $env) = @_;
+  if($env->{HTTP_X_FORWARDED_HOST}) {
+    return $self->key_prefix."_".$env->{HTTP_X_FORWARDED_HOST};
+  }
+  return $self->SUPER::client_identifier($env);
+}
+
 1;
