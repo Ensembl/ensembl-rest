47be10c856f33d60896a49e5de3788c7c4f4be98
Kieron Taylor <ktaylor@ebi.ac.uk>
2013-02-19 17:29:19 +0000

    Addition of get_genetree_by_member_id method.

--- lib/EnsEMBL/REST/Controller/GeneTree.pm
+++ lib/EnsEMBL/REST/Controller/GeneTree.pm
@@ -28,6 +28,32 @@ sub get_genetree : Chained('/') PathPart('genetree/id') Args(1) ActionClass('RES
   $self->status_ok( $c, entity => $gt );
 }
 
+sub get_genetree_by_member_id_GET { }
+
+sub get_genetree_by_member_id : Chained('/') PathPart('genetree/member/id') Args(1) ActionClass('REST') {
+  my ($self, $c, $id) = @_;
+   
+  my $compara_name = $c->request->parameters->{compara}; 
+  my $reg = $c->model('Registry');
+  
+  my ($species, $type, $db) = $c->model('Lookup')->find_object_location($c, $id);
+  
+  $c->go('ReturnError', 'custom', ["Unable to find given object: $id"]) unless $species;
+  
+  my $r = $c->request;
+    
+  my $dba = $reg->get_best_compara_DBAdaptor($c,$species,$compara_name);
+  my $ma = $dba->get_MemberAdaptor;
+  my $member = $ma->fetch_by_source_stable_id('ENSEMBLGENE',$id);
+  $c->go('ReturnError', 'custom', ["Could not fetch GeneTree Member"]) unless $member;
+  
+  my $gta = $dba->get_GeneTreeAdaptor;
+  my $gt = $gta->fetch_default_for_Member($member);
+  $c->go('ReturnError', 'custom', ["Could not fetch GeneTree"]) unless $gt;
+  
+  $self->status_ok( $c, entity => $gt);
+}
+
 __PACKAGE__->meta->make_immutable;
 
 1;
