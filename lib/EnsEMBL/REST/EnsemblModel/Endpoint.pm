5c50185c31a773f83b8c6677bef0a137e08774ec
Andrew Yates <ayates@ebi.ac.uk>
2013-03-02 02:10:05 +0000

    Fixed a number of W3C validation errors. Turned the endpoint into an object. Split parameters into required and optional to aid users to decipher this. User guide fleshed out a little bit more

---
+++ lib/EnsEMBL/REST/EnsemblModel/Endpoint.pm
@@ -0,0 +1,36 @@
+package EnsEMBL::REST::EnsemblModel::Endpoint;
+
+use Moose;
+
+has 'description' => ( isa => 'Str', is => 'ro', required => 1 );
+has 'endpoint'    => ( isa => 'Str', is => 'ro', required => 1 );
+has 'method'      => ( isa => 'Str', is => 'ro', required => 1 );
+has 'group'       => ( isa => 'Str', is => 'ro', required => 1 );
+has 'output'      => ( isa => 'ArrayRef', is => 'ro', required => 1, coerce => 1 );
+has 'params'      => ( isa => 'HashRef', is => 'ro', required => 0 );
+has 'examples'    => ( isa => 'HashRef', is => 'ro', required => 0 );
+
+sub has_required_params {
+  my ($self) = @_;
+  my $p = $self->params();
+  return 0 unless $p;
+  foreach my $key (keys %{$p}) {
+    my $value = $p->{$key};
+    # warn $key .' '.$value->{required};
+    return 1 if $value->{required};
+  }
+  return 0;
+}
+
+sub has_optional_params {
+  my ($self) = @_;
+  my $p = $self->params();
+  return 0 unless $p;
+  foreach my $key (keys %{$p}) {
+    my $value = $p->{$key};
+    return 1 if ! $value->{required};
+  }
+  return 0;
+}
+
+1;
