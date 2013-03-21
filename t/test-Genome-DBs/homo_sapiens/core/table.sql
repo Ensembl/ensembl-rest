77452eca8269dcd86fb606b14af7d8c91ef751c9
Andrew Yates <ayates@ebi.ac.uk>
2013-03-21 12:55:55 +0000

    updating schemas to 71

--- t/test-Genome-DBs/homo_sapiens/core/table.sql
+++ t/test-Genome-DBs/homo_sapiens/core/table.sql
@@ -371,6 +371,7 @@ CREATE TABLE `mapping_set` (
   `mapping_set_id` int(10) unsigned NOT NULL,
   `internal_schema_build` varchar(20) NOT NULL,
   `external_schema_build` varchar(20) NOT NULL,
+  PRIMARY KEY (`mapping_set_id`),
   UNIQUE KEY `mapping_idx` (`internal_schema_build`,`external_schema_build`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -430,7 +431,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`),
   KEY `species_value_idx` (`species_id`,`meta_value`)
-) ENGINE=MyISAM AUTO_INCREMENT=1781 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=1783 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `meta_coord` (
   `table_name` varchar(40) NOT NULL,
