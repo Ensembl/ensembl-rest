24113f4738b93dd9c969f172c4c052e8c0313b05
Andrew Yates <ayates@ebi.ac.uk>
2013-08-29 13:04:48 +0100

    [ENSCORESW-645]. Supporting external db retrieval.
    We can now let users query for an external db. This allows for querying by a species and
    using filters just like other endpoints that support external_db params

---
+++ lib/EnsEMBL/REST/EnsemblModel/ExternalDB.pm
@@ -0,0 +1,94 @@
+package EnsEMBL::REST::EnsemblModel::ExternalDB;
+
+=pod
+
+=head1 NAME
+
+EnsEMBL::REST::EnsemblModel::ExternalDB
+ 
+=head1 DESCRIPTION
+
+Provides access to all external DBs in a given database. Please use the
+C<EnsEMBL::REST::EnsemblModel::ExternalDB::get_ExternalDBs()> method
+to populate the object.
+
+=cut
+
+use Moose;
+
+=head2 ATTRIBUTES
+
+=over 8
+
+=item id - Integer Internal ID of the ExternalDB
+
+=item name - String Name for the ExternalDB. Used in any endpoint when an external_db param is requested
+
+=item release - Maybe[String] Optional release information of the ExternalDB
+
+=item display_name - String Human readable name of the ExternalDB
+
+=item description - Maybe[String] Verbose description of the ExternalDB
+
+=back
+
+=cut
+
+has 'id'            => ( isa => 'Int', is => 'ro', required => 1);
+has 'name'          => ( isa => 'Str', is => 'ro', required => 1 );
+has 'release'       => ( isa => 'Maybe[Str]', is => 'ro', required => 1 );
+has 'display_name'  => ( isa => 'Str', is => 'ro', required => 1 );
+has 'description'   => ( isa => 'Maybe[Str]', is => 'ro', required => 1 );
+
+=head2 get_ExternalDBs
+
+  Args [1]    : DBAdaptor DBAdaptor to lookup external dbs in. Must be a core-like schema
+  Args [2]    : String Filter pattern to apply to the query. Supports SQL like statements
+  Example     : my $external_dbs = EnsEMBL::REST::EnsemblModel::ExternalDB->get_ExternalDBs($dba);
+                my $go_external_dbs = EnsEMBL::REST::EnsemblModel::ExternalDB->get_ExternalDBs($dba, 'GO%');
+  Description : Finds all external DBs in the given species' core database
+  Returntype  : ArrayRef of ExternalDB objects
+  Exceptions  : SQL based exceptions
+
+=cut
+
+sub get_ExternalDBs {
+  my ($class, $dba, $filter) = @_;
+  assert_ref($dba, 'Bio::EnsEMBL::DBSQL::DBAdaptor', 'DBAdaptor');
+  $filter ||= '%';
+  my $sql = <<'SQL';
+select external_db_id, db_name, db_release, db_display_name, description
+from external_db
+where db_name like ?
+SQL
+  my $results = $dba->dbc->sql_helper->execute(-SQL => $sql, -PARAMS => [$filter], -CALLBACK => sub {
+    my ($row) = @_;
+    my ($id, $name, $release, $display_name, $description) = @{$row};
+    return $class->new(
+      id => $id, name => $name, release => $release, 
+      display_name => $display_name, description => $description
+    );
+  });
+  return $results;
+}
+
+=head2 summary_as_hash
+
+  Example     : my $hash = $obj->summary_as_hash();
+  Description : Converts the current object into an unblessed hash fit for
+                serialisation purposes
+  Returntype  : HashRef Basic representation of the current object
+
+=cut
+
+sub summary_as_hash {
+  my ($self) = @_;
+  return { 
+    name          => $self->name(), 
+    release       => $self->release(), 
+    display_name  => $self->display_name(), 
+    description   => $self->description() 
+  };
+}
+
+1;
