f07822c053333570d3c9296433a46ba5f1248ed4
Andrew Yates <ayates@ebi.ac.uk>
2013-08-29 13:21:09 +0100

    [ENSCORES-646]. Adding querying support for biotypes.
    Users can now query for biotypes. We return the DBs and
    biotypes they apply to.

---
+++ lib/EnsEMBL/REST/EnsemblModel/Biotype.pm
@@ -0,0 +1,96 @@
+package EnsEMBL::REST::EnsemblModel::Biotype;
+
+=pod
+
+=head1 NAME
+
+EnsEMBL::REST::EnsemblModel::Biotype
+ 
+=head1 DESCRIPTION
+
+Provides access to all biotypes in a given species for both genes and
+transcripts. The code will loop through all available GeneAdaptor instances
+in the registry as a guide to applicable DBs. Please use the
+C<EnsEMBL::REST::EnsemblModel::Biotype::get_Biotypes()> method
+to populate the object.
+
+=cut
+
+use Moose;
+use Bio::EnsEMBL::Utils::Scalar qw/assert_ref/;
+
+=head2 ATTRIBUTES
+
+=over 8
+
+=item biotype - String name of the biotype
+
+=item groups - ArrayRef[String] Groups this biotype is found in
+
+=item objects - ArrayRef[String] The object type this biotype was found in
+
+=back
+
+=cut
+
+has 'biotype' => ( isa => 'Str', is => 'ro', required => 1);
+has 'groups'  => ( isa => 'HashRef[Str]', is => 'ro', required => 0, default => sub {{}});
+has 'objects' => ( isa => 'HashRef[Str]', is => 'ro', required => 0, default => sub {{}});
+
+=head2 get_Biotypes
+
+  Args [1]    : Catalyst Catalyst context instance to use for lookup
+  Args [2]    : String Species name to lookup
+  Example     : my $biotypes = EnsEMBL::REST::EnsemblModel::Biotype->get_Biotypes($c, 'human');
+  Description : Loops through all available GeneAdaptor instances for the given species and
+                queries for distinct biotypes in the gene and transcript table.
+  Returntype  : ArrayRef of Biotype objects
+  Exceptions  : SQL based exceptions
+
+=cut
+
+sub get_Biotypes {
+  my ($class, $c, $species) = @_;
+  assert_ref($c, 'Catalyst', 'Catalyst Context');
+  my %biotypes;
+  my $adaptors = $c->model('Registry')->get_all_adaptors_by_type($species, 'gene');
+  foreach my $adaptor (@{$adaptors}) {
+    my $dba = $adaptor->db();
+    my $dbc = $dba->dbc();
+    foreach my $object (qw/gene transcript/) {
+      my $sql = "select distinct biotype from $object";
+      $dbc->sql_helper->execute_no_return(-SQL => $sql, -CALLBACK => sub {
+        my ($row) = @_;
+        my ($biotype) = @{$row};
+        my $biotype_obj = $biotypes{$biotype};
+        if(! $biotype_obj) {
+          $biotype_obj = $class->new(biotype => $biotype);
+          $biotypes{$biotype} = $biotype_obj;
+        }
+        $biotype_obj->groups()->{$dba->group} = 1;
+        $biotype_obj->objects()->{$object} = 1;
+      });  
+    }
+  }
+  return [values %biotypes];
+}
+
+=head2 summary_as_hash
+
+  Example     : my $hash = $obj->summary_as_hash();
+  Description : Converts the current object into an unblessed hash fit for
+                serialisation purposes
+  Returntype  : HashRef Basic representation of the current object
+
+=cut
+
+sub summary_as_hash {
+  my ($self) = @_;
+  return { 
+    biotype => $self->biotype(), 
+    groups  => [ sort keys %{$self->groups()}],
+    objects => [ sort keys %{$self->objects()}],
+  };
+}
+
+1;
