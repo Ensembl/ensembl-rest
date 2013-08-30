161f9fa6698d2c39e1ca1fc1f5d89fabbea77505
Andrew Yates <ayates@ebi.ac.uk>
2013-08-30 10:46:59 +0100

    [ENSCORESW-648]. Cleaning up compara info endpoints.
    Removed 2 extra endpoints and pushed their data into a single info endpoint. This
    feels more coherent. Documentation has been updated accordingly. Added more user
    config variables.

--- lib/EnsEMBL/REST/Controller/Compara.pm
+++ lib/EnsEMBL/REST/Controller/Compara.pm
@@ -11,6 +11,8 @@ BEGIN { extends 'Catalyst::Controller::REST'; }
 my $FORMAT_LOOKUP = { full => '_full_encoding', condensed => '_condensed_encoding' };
 my $TYPE_TO_COMPARA_TYPE = { orthologues => 'ENSEMBL_ORTHOLOGUES', paralogues => 'ENSEMBL_PARALOGUES', all => ''};
 
+has default_compara => ( is => 'ro', isa => 'Str', default => 'multi' );
+
 sub get_adaptors :Private {
   my ($self, $c) = @_;
 
@@ -290,79 +292,6 @@ sub _decode_members {
   }
   return ($src, $trg);
 }
-
-sub genomic_methods_GET { }
-
-sub genomic_methods : Chained('/') PathPart('compara/info/methods') Args(0) ActionClass('REST') {
-  my ($self, $c) = @_;
-
-  my $class = "GenomicAlign%";
-  my $types;
-
-  try {
-    my $methods = $c->model('Lookup')->find_compara_methods($class);
-    
-    #return the method types
-    foreach my $method (@$methods) {
-      push @$types, $method->type;
-    }
-  } catch {
-    $c->go( 'ReturnError', 'from_ensembl', [$_] );
-  };
-
-  $self->status_ok($c, entity => $types);
-
-}
-
-sub method_and_species_sets_GET { }
-
-sub method_and_species_sets : Chained('/') PathPart("compara/info/method_and_species_sets") Args(1) ActionClass('REST') {
-  my ($self, $c, $method) = @_;
-
-  my $method_link_species_sets;
-  try {
-    $method_link_species_sets = $c->model('Lookup')->find_compara_method_link_species_sets($method);
-  } catch {
-    $c->go( 'ReturnError', 'from_ensembl', [$_] );
-  };
-
-  $self->status_ok($c, entity => $method_link_species_sets);
-
-}
-
-
-sub species_set_groups_GET { }
-
-sub species_set_groups : Chained('/') PathPart("compara/info/species_set_groups") Args(1) ActionClass('REST') {
-  my ($self, $c, $method) = @_;
-
-  my $species_set_groups;
-  try {
-    $species_set_groups = $c->model('Lookup')->find_compara_species_set_groups($method);
-  } catch {
-    $c->go( 'ReturnError', 'from_ensembl', [$_] );
-  };
-
-  $self->status_ok($c, entity => $species_set_groups);
-
-}
-
-sub species_sets_GET { }
-
-sub species_sets : Chained('/') PathPart("compara/info/species_sets") Args(1) ActionClass('REST') {
-  my ($self, $c, $method) = @_;
-
-  my $species_sets;
-  try {
-    $species_sets = $c->model('Lookup')->find_compara_species_sets($method);
-  } catch {
-    $c->go( 'ReturnError', 'from_ensembl', [$_] );
-  };
-
-  $self->status_ok($c, entity => $species_sets);
-
-}
-
 
 __PACKAGE__->meta->make_immutable;
 
