21636c06664f0e51cc384a3184cfc2f5ad66bd99
Andrew Yates <ayates@ebi.ac.uk>
2013-09-02 12:46:48 +0100

    [ENSCORESW-649]. Retry-After header is now set once limits are burst.
    Once we exceed the max requests per time period we set a Retry-After
    header in seconds (including fractions). This allows users to listen
    for a bad request, sleep for the period specified and then retry. Test
    cases have been written which test for these bursts.

--- lib/Plack/Middleware/Throttle/ReLimiter.pm
+++ lib/Plack/Middleware/Throttle/ReLimiter.pm
@@ -3,6 +3,11 @@ package Plack::Middleware::Throttle::ReLimiter;
 use Moose;
 extends 'Plack::Middleware::Throttle::Limiter';
 
+# Provides a way of controlling the retry after env variable by
+# adding a constant amount of time. This is useful if you are using
+# the per-second limiter as a way of controlling per second bursts
+has 'retry_after_addition' => ( isa => 'Num', is => 'ro', lazy => 1, default => 0 );
+
 use Plack::Util qw//;
 use Scalar::Util qw//;
 
@@ -24,5 +29,15 @@ sub client_identifier {
   }
   return $self->SUPER::client_identifier($env);
 }
+
+override 'over_rate_limit' => sub {
+  my ($self) = @_;
+  my $res = super();
+  my $headers = $res->[1];
+  my $reset_time = $self->reset_time();
+  $reset_time += $self->retry_after_addition();
+  Plack::Util::header_set( $headers, 'Retry-After', $reset_time );
+  return $res;
+};
 
 1;
