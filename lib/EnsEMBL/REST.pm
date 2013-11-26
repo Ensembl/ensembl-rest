73a05b4044b6becfc31d3f3e4ba3bb17398e19e7
Andrew Yates <ayates@ebi.ac.uk>
2013-11-26 12:19:49 +0000

    [ENSCORESW-771]. Ensembl is now distributed under an Apache 2.0 license and not Apache-like

--- lib/EnsEMBL/REST.pm
+++ lib/EnsEMBL/REST.pm
@@ -1,3 +1,21 @@
+=head1 LICENSE
+
+Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
 =pod
 
 =head1 NAME
@@ -7,16 +25,6 @@ EnsEMBL::REST - A RESTful API for the access of data from Ensembl and Ensembl co
 =head1 AUTHOR
 
 The EnsEMBL Group - helpdesk@ensembl.org
-
-=head1 LICENSE
-
-  Copyright (c) 1999-2013 The European Bioinformatics Institute and
-  Genome Research Limited.  All rights reserved.
-
-  This software is distributed under a modified Apache license.
-  For license details, please see
-
-    http://www.ensembl.org/info/about/code_licence.html
 
 =cut
 
