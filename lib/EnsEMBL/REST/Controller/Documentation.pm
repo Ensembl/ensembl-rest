bbed9e03f5d942c88ef76c645ba9457e9f0d8d39
Kieron Taylor <ktaylor@ebi.ac.uk>
2014-06-02 14:21:26 +0100

    Alterations to documentation generator to support multiple method types (GET + POST). Specification of message format still needs refinement.

--- lib/EnsEMBL/REST/Controller/Documentation.pm
+++ lib/EnsEMBL/REST/Controller/Documentation.pm
@@ -53,7 +53,7 @@ sub info :Path('info') :Args(1) {
   if($endpoint_cfg) {
     $endpoint_cfg = $c->model('Documentation')->enrich($endpoint_cfg);
     $c->stash()->{endpoint} = $endpoint_cfg;
-    $c->stash()->{template_title} = $endpoint_cfg->{method} . ' ' . $endpoint_cfg->{endpoint};
+    $c->stash()->{template_title} = join(',',@{ $endpoint_cfg->{method} } ) . ' ' . $endpoint_cfg->{endpoint};
   }
   else {
     $c->response->status(404);
