ac99f1d34aaf2621e2ade6189035c42e609fea6f
Magali Ruffier <mr6@ebi.ac.uk>
2014-06-03 14:17:12 +0100

    ENSCORESW-740 and ENSCORESW-769: new variation endpoint returns basic information about a variation id, including location and maf additional options include genotype and population information

---
+++ lib/EnsEMBL/REST/Model/Variation.pm
@@ -0,0 +1,148 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::REST::Model::Variation;
+
+use Moose;
+extends 'Catalyst::Model';
+
+with 'Catalyst::Component::InstancePerContext';
+
+has 'context' => (is => 'ro');
+
+sub build_per_context_instance {
+  my ($self, $c, @args) = @_;
+  return $self->new({ context => $c, %$self, @args });
+}
+
+sub fetch_variation {
+  my ($self, $variation_id) = @_;
+
+  my $c = $self->context();
+  my $species = $c->stash->{species};
+
+  $c->go('ReturnError', 'custom', ["No variation given. Please specify a variation to retrieve from this service"]) if ! $variation_id;
+
+  my $vfa = $c->model('Registry')->get_adaptor($species, 'Variation', 'Variation');
+  my $variation = $vfa->fetch_by_name($variation_id);
+  if (!$variation) {
+    $c->go('ReturnError', 'custom', ["$variation_id not found for $species"]);
+  }
+  return $self->to_hash($variation);
+}
+
+
+sub to_hash {
+  my ($self, $variation) = @_;
+  my $c = $self->context();
+  my $variation_hash;
+
+  $variation_hash->{name} = $variation->name,
+  $variation_hash->{source} = $variation->source_description,
+  $variation_hash->{ambiguity} = $variation->ambig_code,
+  $variation_hash->{synonyms} = $variation->get_all_synonyms,
+  $variation_hash->{ancestral_allele} = $variation->ancestral_allele,
+  $variation_hash->{var_class} = $variation->var_class,
+  $variation_hash->{most_severe_consequence} = $variation->display_consequence,
+  $variation_hash->{MAF} = $variation->minor_allele_frequency,
+  $variation_hash->{evidence} = $variation->get_all_evidence_values();
+  $variation_hash->{clinical_significance} = $variation->get_all_clinical_significance_states() if @{$variation->get_all_clinical_significance_states()};
+  $variation_hash->{mappings} = $self->VariationFeature($variation);
+  $variation_hash->{populations} = $self->Alleles($variation) if $c->request->param('pops');
+  $variation_hash->{genotypes} = $self->Genotypes($variation) if $c->request->param('genotypes');
+
+  return $variation_hash;
+}
+
+sub VariationFeature {
+  my ($self, $variation) = @_;
+
+  my @mappings;
+  my $vfs = $variation->get_all_VariationFeatures();
+  foreach my $vf (@$vfs) {
+    push (@mappings, $self->vf_as_hash($vf));
+  }
+  return \@mappings;
+}
+
+sub vf_as_hash {
+  my ($self, $vf) = @_;
+
+  my $variation_feature;
+  $variation_feature->{location} = $vf->seq_region_name . ":" . $vf->seq_region_start . "-" . $vf->seq_region_end;
+  $variation_feature->{allele_string} = $vf->allele_string;
+
+  return $variation_feature;
+}
+
+sub Genotypes {
+  my ($self, $variation) = @_;
+
+  my @genotypes;
+  my $genotypes = $variation->get_all_IndividualGenotypes;
+  foreach my $gen (@$genotypes) {
+    push (@genotypes, $self->gen_as_hash($gen));
+  }
+  return \@genotypes;
+}
+
+sub gen_as_hash {
+  my ($self, $gen) = @_;
+
+  my $gen_hash;
+  $gen_hash->{genotype} = $gen->genotype_string();
+  $gen_hash->{individual} = $gen->individual->name();
+  $gen_hash->{gender} = $gen->individual->gender();
+  $gen_hash->{submission_id} = $gen->subsnp() if $gen->subsnp;
+
+  return $gen_hash;
+}
+
+sub Alleles {
+  my ($self, $variation) = @_;
+
+  my @populations;
+  my $alleles = $variation->get_all_Alleles();
+  foreach my $allele (@$alleles) {
+    if ($allele->frequency) {
+      push (@populations, $self->pops_as_hash($allele));
+    }
+  }
+
+  return \@populations;
+}
+
+sub pops_as_hash {
+  my ($self, $allele) = @_;
+
+  my $population;
+  $population->{frequency} = $allele->frequency();
+  $population->{population} = $allele->population->name();
+  $population->{allele_count} = $allele->count();
+  $population->{allele} = $allele->allele();
+  $population->{submission_id} = $allele->subsnp() if $allele->subsnp();
+
+  return $population;
+}
+
+
+with 'EnsEMBL::REST::Role::Content';
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/variation.t
@@ -0,0 +1,62 @@
+# Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http=>//www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use strict;
+use warnings;
+
+BEGIN {
+  use FindBin qw/$Bin/;
+  use lib "$Bin/lib";
+  use RestHelper;
+  $ENV{CATALYST_CONFIG} = "$Bin/../ensembl_rest_testing.conf";
+  $ENV{ENS_REST_LOG4PERL} = "$Bin/../log4perl_testing.conf";
+}
+
+use Test::More;
+use Test::Differences;
+use Catalyst::Test ();
+use Bio::EnsEMBL::Test::MultiTestDB;
+use Bio::EnsEMBL::Test::TestUtils;
+
+my $dba = Bio::EnsEMBL::Test::MultiTestDB->new('homo_sapiens');
+my $multi = Bio::EnsEMBL::Test::MultiTestDB->new('multi');
+Catalyst::Test->import('EnsEMBL::REST');
+
+my $base = '/variation/homo_sapiens';
+
+#Get basic variation summary
+  my $id = 'rs142276873';
+  my $json = json_GET("$base/$id", 'Variation feature');
+  #is(scalar(@$json), 1, '1 variation feature returned');
+  my $expected_variation = {source => 'Variants (including SNPs and indels) imported from dbSNP', name => $id, MAF => '0.123049', ambiguity => 'R', var_class => 'SNP', synonyms => [], evidence => ['Multiple_observations','1000Genomes'], ancestral_allele => 'G', most_severe_consequence => 'Intron variant', mappings => [{"location"=>"18:23821095-23821095","allele_string"=>"G/A"}]};
+  eq_or_diff($json, $expected_variation, "Checking the result from the variation endpoint");
+  
+# Get additional genotype information
+  $id = 'rs67521280';
+  $expected_variation = {source => 'Variants (including SNPs and indels) imported from dbSNP', name => $id, MAF => undef, ambiguity => undef, var_class => 'indel', synonyms => [], evidence => [], ancestral_allele => undef, most_severe_consequence => 'Intergenic variant', mappings => [{"location"=> "11:6303493-6303493","allele_string"=>"-/GT"}] };
+  my $expected_genotype = { %{$expected_variation}, 
+  genotypes => [{genotype => "GT|GT", gender => "Male", individual => "J. CRAIG VENTER", submission_id => 'ss95559393'}] };
+  my $genotype_json = json_GET("$base/$id?genotypes=1", "Genotype info");
+  eq_or_diff($genotype_json, $expected_genotype, "Returning genotype information");
+
+
+# Include population information
+  my $expected_pops = { %{$expected_variation},
+  populations => [{population => "HUMANGENOME_JCVI:J. Craig Venter",frequency => "1",allele => "GT",allele_count => "2", submission_id => 'ss95559393'}]};
+  my $pops_json = json_GET("$base/$id?pops=1", "Population info");
+  eq_or_diff($pops_json, $expected_pops, "Returning population information");
+
+
+
+done_testing();
