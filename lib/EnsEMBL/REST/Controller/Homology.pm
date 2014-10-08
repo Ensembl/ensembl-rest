f6c750b969593691ca3234b6b1dbbe31fb6a361c
Matthieu Muffato <muffato@ebi.ac.uk>
2014-10-08 12:07:35 +0100

    Renamed the Compara controller to Homology

--- lib/EnsEMBL/REST/Controller/Homology.pm
+++ lib/EnsEMBL/REST/Controller/Homology.pm
@@ -16,7 +16,7 @@ limitations under the License.
 
 =cut
 
-package EnsEMBL::REST::Controller::Compara;
+package EnsEMBL::REST::Controller::Homology;
 use Moose;
 use namespace::autoclean;
 use Try::Tiny;
