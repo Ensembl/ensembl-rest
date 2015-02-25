e73d470cf426096509d15c482fea01e79abddcf7
Sarah Hunt <seh@ebi.ac.uk>
2015-02-25 16:11:17 +0000

    tweak travis environment

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -2,6 +2,8 @@
 
 export PERL5LIB=$PWD/bioperl-live-bioperl-release-1-2-3:$PWD/ensembl-test/modules:$PWD/ensembl/modules:$PWD/ensembl-compara/modules:$PWD/ensembl-variation/modules:$PWD/ensembl-funcgen/modules:$PWD/ensembl-io/modules:$PWD/lib
 
+export PATH=$PATH:$PWD/tabix
+
 echo "Running test suite"
 if [ "$COVERALLS" = 'true' ]; then
   PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test' perl $PWD/ensembl-test/scripts/runtests.pl -verbose t $SKIP_TESTS
