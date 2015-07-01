c68015025c51991cf8306e50d76fad1dd400b079
Magali Ruffier <mr6@ebi.ac.uk>
2015-07-01 11:14:30 +0100

    added ensembl-io to list of required checkouts

--- bin/production/start_rest_server.sh
+++ bin/production/start_rest_server.sh
@@ -33,7 +33,7 @@ fi
 APP_HOME=$(cd $SCRIPT_DIR/../../ && pwd)
 
 # Library path work
-for ensdir in ensembl-variation ensembl-funcgen ensembl-compara ensembl; do
+for ensdir in ensembl-variation ensembl-funcgen ensembl-compara ensembl ensembl-io; do
   PERL5LIB=$ENS_GIT_ROOT_DIR/$ensdir/modules:$PERL5LIB
 done
 PERL5LIB=$APP_HOME/../bioperl-live:$PERL5LIB
