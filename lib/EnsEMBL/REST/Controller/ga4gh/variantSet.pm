453fdbcf88bbe5934f46714913a116b64189e226
Sarah Hunt <seh@ebi.ac.uk>
2015-11-16 12:57:51 +0000

    update to v0.6

--- lib/EnsEMBL/REST/Controller/ga4gh/variantSet.pm
+++ lib/EnsEMBL/REST/Controller/ga4gh/variantSet.pm
@@ -28,12 +28,14 @@ EnsEMBL::REST->turn_on_config_serialisers(__PACKAGE__);
 
 =pod
 
-POST requests : /variantsets/
+POST requests : /variantsets/search -d
 
 { "dataSetIds": [1],
   "pageToken":  null,
   "pageSize": 10
 }
+
+GET requests : /variantsets/:id
 
 application/json
 
@@ -48,26 +50,47 @@ sub get_request_POST {
 }
 
 
-sub get_request: Chained('/') PathPart('ga4gh/variantsets') ActionClass('REST')  {
+sub get_request: Chained('/') PathPart('ga4gh/variantsets/search') ActionClass('REST')  {
   my ( $self, $c ) = @_;
   my $post_data = $c->req->data;
 
   #$c->log->debug(Dumper $post_data);
+
+  $c->go( 'ReturnError', 'custom', [ ' Cannot find "datasetId" key in your request'])
+    unless exists $post_data->{datasetId};
+
   ## set a default page size if not supplied or not a number
   $post_data->{pageSize} = 10 unless (defined  $post_data->{pageSize} &&  
                                       $post_data->{pageSize} =~ /\d+/ &&
                                       $post_data->{pageSize} >0  );
 
-  my $gavariantSet;
+  my $variantSets;
 
   try {
-    $gavariantSet = $c->model('ga4gh::variantSet')->fetch_ga_variantSet($post_data);
+    $variantSets = $c->model('ga4gh::variantSet')->fetch_variantSets($post_data);
   } catch {
     $c->go('ReturnError', 'from_ensembl', [$_]);
   };
 
-  $self->status_ok($c, entity => $gavariantSet);
+  $self->status_ok($c, entity => $variantSets);
 }
+
+
+
+sub id: Chained('/') PathPart('ga4gh/variantsets') ActionClass('REST') {}
+
+sub id_GET {
+  my ($self, $c, $id) = @_;
+  my $variantSet;
+  try {
+    $variantSet = $c->model('ga4gh::variantSet')->getVariantSet($id);
+  } catch {
+    $c->go('ReturnError', 'from_ensembl', [qq{$_}]) if $_ =~ /STACK/;
+    $c->go('ReturnError', 'custom', [qq{$_}]);
+  };
+  $self->status_ok($c, entity => $variantSet);
+}
+
 
 
 __PACKAGE__->meta->make_immutable;
