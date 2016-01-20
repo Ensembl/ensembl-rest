830b6cd1bc5500c6bebecd96a6fe7957edd46811
Thomas Juettemann <juettemann@users.noreply.github.com>
2016-01-20 11:28:43 +0000

    Update Preload.pm

--- Preload.pm
+++ Preload.pm
@@ -1,5 +1,5 @@
 =head1 LICENSE
-Copyright [2009-2014] EMBL-European Bioinformatics Institute
+Copyright [2009-2016] EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
