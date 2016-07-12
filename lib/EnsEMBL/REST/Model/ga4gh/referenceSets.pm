f32485f1f26a71f9ee04001a4fa3da2bc8bcf9cd
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-07-12 14:28:09 +0100

    Revert "copyright updated to EBI"
    This reverts commit 6c9d97fcf28609710f0fd2a89a279ec552aadc74.

--- lib/EnsEMBL/REST/Model/ga4gh/referenceSets.pm
+++ lib/EnsEMBL/REST/Model/ga4gh/referenceSets.pm
@@ -1,6 +1,5 @@
 =head1 LICENSE
-Copyright [2016] EMBL-European Bioinformatics Institute
-Copyright [2016] EMBL-European Bioinformatics Institute
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
