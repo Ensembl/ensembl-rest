7286f7cb84ceef66859ddf1024e52d0df1eaf496
Kieron Taylor <ktaylor@ebi.ac.uk>
2016-07-14 13:42:47 +0100

    Reconfigure for the post-eqtl era. Worker count dropped to reduce peak memory loads

--- bin/production/start_rest_server.sh
+++ bin/production/start_rest_server.sh
@@ -34,7 +34,7 @@ fi
 APP_HOME=$(cd $SCRIPT_DIR/../../ && pwd)
 
 # Library path work
-for ensdir in ensembl-variation ensembl-funcgen ensembl-compara ensembl ensembl-io; do
+for ensdir in ensembl-variation ensembl-funcgen ensembl-compara ensembl ensembl-io ensembl-hdf5; do
   PERL5LIB=$ENS_GIT_ROOT_DIR/$ensdir/modules:$PERL5LIB
 done
 PERL5LIB=$APP_HOME/../bioperl-live:$PERL5LIB
@@ -47,7 +47,7 @@ export PERL5LIB
 export ENS_REST_LOG4PERL=$APP_HOME'/configurations/production/log4perl.conf'
 
 # Server settings for starman
-WORKERS=15
+WORKERS=10
 BACKLOG=1024
 MAXREQUESTS=10000
 RESTART_INTERVAL=1
