3b6a787cfb449aeefb5226117c3eebe012e0156e
Magali Ruffier <mr6@ebi.ac.uk>
2016-09-15 17:15:40 +0100

    some schema patching

--- t/test-genome-DBs/multi/compara/table.sql
+++ t/test-genome-DBs/multi/compara/table.sql
@@ -133,12 +133,6 @@ CREATE TABLE `gene_member` (
   `dnafrag_end` int(10) DEFAULT NULL,
   `dnafrag_strand` tinyint(4) DEFAULT NULL,
   `display_label` varchar(128) DEFAULT NULL,
-  `families` tinyint(1) unsigned DEFAULT '0',
-  `gene_trees` tinyint(1) unsigned DEFAULT '0',
-  `gene_gain_loss_trees` tinyint(1) unsigned DEFAULT '0',
-  `orthologues` int(10) unsigned DEFAULT '0',
-  `paralogues` int(10) unsigned DEFAULT '0',
-  `homoeologues` int(10) unsigned DEFAULT '0',
   PRIMARY KEY (`gene_member_id`),
   UNIQUE KEY `stable_id` (`stable_id`),
   KEY `taxon_id` (`taxon_id`),
@@ -148,6 +142,18 @@ CREATE TABLE `gene_member` (
   KEY `dnafrag_id_start` (`dnafrag_id`,`dnafrag_start`),
   KEY `dnafrag_id_end` (`dnafrag_id`,`dnafrag_end`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 MAX_ROWS=100000000;
+
+CREATE TABLE `gene_member_hom_stats` (
+  `gene_member_id` int(10) unsigned NOT NULL,
+  `collection` varchar(40) NOT NULL,
+  `families` int(10) unsigned NOT NULL DEFAULT '0',
+  `gene_trees` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `gene_gain_loss_trees` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `orthologues` int(10) unsigned NOT NULL DEFAULT '0',
+  `paralogues` int(10) unsigned NOT NULL DEFAULT '0',
+  `homoeologues` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`gene_member_id`,`collection`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 CREATE TABLE `gene_tree_node` (
   `node_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -349,6 +355,7 @@ CREATE TABLE `homology` (
   `gene_tree_root_id` int(10) unsigned DEFAULT NULL,
   `goc_score` tinyint(3) unsigned DEFAULT NULL,
   `wga_coverage` decimal(5,2) DEFAULT NULL,
+  `is_high_confidence` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`homology_id`),
   KEY `method_link_species_set_id` (`method_link_species_set_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
@@ -421,7 +428,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`(255)),
   KEY `species_value_idx` (`species_id`,`meta_value`(255))
-) ENGINE=MyISAM AUTO_INCREMENT=71 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `method_link` (
   `method_link_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
