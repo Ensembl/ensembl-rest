ecb0fd67844deaed61a0b61bf3ba593fa5beaeaf
Matthieu Muffato <muffato@ebi.ac.uk>
2016-10-03 23:26:32 +0100

    MethodLinkSpeciesSet::species_set_obj() is deprecated

--- lib/EnsEMBL/REST/Model/Lookup.pm
+++ lib/EnsEMBL/REST/Model/Lookup.pm
@@ -148,7 +148,7 @@ sub find_compara_species_sets {
   my $mlsss = $compara_dba->get_MethodLinkSpeciesSetAdaptor->fetch_all_by_method_link_type($method);
   foreach my $mlss (@$mlsss) {
     my $species_set = {};
-    my $species_set_obj = $mlss->species_set_obj();
+    my $species_set_obj = $mlss->species_set();
     my @species_set_genomes = map { $_->name } @{$species_set_obj->genome_dbs};
     $species_set->{species_set_group} = $species_set_obj->name;
     $species_set->{name} = $mlss->name;
