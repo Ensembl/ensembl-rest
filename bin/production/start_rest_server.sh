2d06a5bc2d0f00ed48da9bb8f4f21aa580a8c4e9
Matthew Laird <lairdm@ebi.ac.uk>
2017-03-30 15:15:03 +0100

    We need a placeholder in the startup script for Ansible to insert PATH and PERL5LIB lines

--- bin/production/start_rest_server.sh
+++ bin/production/start_rest_server.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# {mark} Ansible Perl Paths
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 PORT=5000
