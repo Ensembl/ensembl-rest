b618a25554fe2786dcd1f60c64c0b2960c21cd92
dglemos <dlemos@ebi.ac.uk>
2019-09-26 10:33:07 +0100

    Fix attrib_id in table variation_citation

--- t/test-genome-DBs/homo_sapiens/variation/table.sql
+++ t/test-genome-DBs/homo_sapiens/variation/table.sql
@@ -608,7 +608,7 @@ CREATE TABLE `variation_attrib` (
 CREATE TABLE `variation_citation` (
   `variation_id` int(10) unsigned NOT NULL,
   `publication_id` int(10) unsigned NOT NULL,
-  `data_source_attrib` set('610','611','612') DEFAULT NULL,
+  `data_source_attrib` set('615','616','617') DEFAULT NULL,
   PRIMARY KEY (`variation_id`,`publication_id`),
   KEY `data_source_attrib_idx` (`data_source_attrib`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
