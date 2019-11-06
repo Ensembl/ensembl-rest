70a3e99ce9c72813bc913b6d57674dba627b5da8
Marek Szuba <marek.szuba@ebi.ac.uk>
2019-11-06 10:49:29 +0000

    Patch test databases
    Source: master branches of all relevant Ensembl repositories as of 10:00
    today. This brings all test databases at hand other than the stable-ID
    one, which remains at 94 on purpose, to schema version 100.

--- t/test-genome-DBs/homology/compara/table.sql
+++ t/test-genome-DBs/homology/compara/table.sql
@@ -429,7 +429,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`(255)),
   KEY `species_value_idx` (`species_id`,`meta_value`(255))
-) ENGINE=MyISAM AUTO_INCREMENT=79 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=81 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `method_link` (
   `method_link_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
