0d4056248cfc2c552048ce54a2a0a2f5e44c0191
Matthieu Muffato <muffato@ebi.ac.uk>
2020-03-19 00:04:57 +0000

    e102 version bump for the Compara databases

--- t/test-genome-DBs/multi/compara/table.sql
+++ t/test-genome-DBs/multi/compara/table.sql
@@ -452,7 +452,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`(255)),
   KEY `species_value_idx` (`species_id`,`meta_value`(255))
-) ENGINE=MyISAM AUTO_INCREMENT=134 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=136 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `method_link` (
   `method_link_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -725,3 +725,4 @@ CREATE TABLE `synteny_region` (
   PRIMARY KEY (`synteny_region_id`),
   KEY `method_link_species_set_id` (`method_link_species_set_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
