219f1312f7e8a952ccdb44682667b503bc9dd9b0
nwillhoft <70575561+nwillhoft@users.noreply.github.com>
2024-06-04 11:37:43 +0100

    Update meta_key value in regulation-related data

--- t/test-genome-DBs/homo_sapiens/funcgen/table.sql
+++ t/test-genome-DBs/homo_sapiens/funcgen/table.sql
@@ -192,7 +192,7 @@ CREATE TABLE `chance` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `data_file` (
-  `data_file_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `data_file_id` int(10) NOT NULL AUTO_INCREMENT,
   `table_id` int(10) unsigned NOT NULL,
   `table_name` varchar(32) NOT NULL,
   `path` varchar(255) NOT NULL,
@@ -396,12 +396,12 @@ CREATE TABLE `idr` (
 CREATE TABLE `meta` (
   `meta_id` int(10) NOT NULL AUTO_INCREMENT,
   `species_id` int(10) unsigned DEFAULT '1',
-  `meta_key` varchar(46) NOT NULL,
-  `meta_value` varchar(950) NOT NULL,
+  `meta_key` varchar(64) NOT NULL,
+  `meta_value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`),
   KEY `species_value_idx` (`species_id`,`meta_value`)
-) ENGINE=MyISAM AUTO_INCREMENT=792 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM AUTO_INCREMENT=793 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `meta_coord` (
   `table_name` varchar(40) NOT NULL,
