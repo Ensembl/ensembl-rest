471f478dbfd77943bda418fde9ddd58b2124c57d
nwillhoft <nwillhoft@ebi.ac.uk>
2025-07-10 16:04:22 +0100

    Update test dbs for 116

--- t/test-genome-DBs/homo_sapiens/core/table.sql
+++ t/test-genome-DBs/homo_sapiens/core/table.sql
@@ -490,7 +490,7 @@ CREATE TABLE `meta` (
   PRIMARY KEY (`meta_id`),
   UNIQUE KEY `species_key_value_idx` (`species_id`,`meta_key`,`meta_value`),
   KEY `species_value_idx` (`species_id`,`meta_value`)
-) ENGINE=MyISAM AUTO_INCREMENT=1892 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=MyISAM AUTO_INCREMENT=1893 DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
 
 CREATE TABLE `meta_coord` (
   `table_name` varchar(40) NOT NULL,
